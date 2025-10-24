5b8a265441bc30c856b478d90f9fbe62274281a4
Akhdan <akhdanrafifdzr@gmail.com>
2025-10-24 19:19:42 +0700

    Fix some package

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    SUPABASE_BUCKET: process.env.SUPABASE_BUCKET,
+  },
   images: {
     unoptimized: true,
   },
